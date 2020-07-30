f25cf73fc4540893fd1b2fc45fbd6c5dbe95c752
Tamir Friedman <tamir.fri@gmail.com>
2020-07-31 00:46:29 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,11 @@
 provider "aws" {
-  version = "2.70.0"
+  region = "eu-central-1"
+  
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-079024c517d22af5b"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = "launch-wizard-1"
+  key_name               = "awskeypair"
 }
